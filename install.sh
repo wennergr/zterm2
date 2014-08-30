6b0eec565e2aa82f91f1beb55f0b899b2954e0b6
Tobias Wennergren <tobias@loyal3.com>
2014-08-29 17:15:30 -0700

    Updated install.sh to take non existing usr/local/bin into account

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+set -e
+
+INSTALL_DIR=/usr/local/bin
+mkdir -p ${INSTALL_DIR}
 
 # Install lrzsz if needed
 brew list -1 | grep lrzsz > /dev/null || brew install lrzsz
 
-sudo install -m 555 ziterm2-recv.sh /usr/local/bin
-sudo install -m 555 ziterm2-send.sh /usr/local/bin
+sudo install -m 555 ziterm2-recv.sh ${INSTALL_DIR}
+sudo install -m 555 ziterm2-send.sh ${INSTALL_DIR}
