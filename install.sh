57f6164f3d1b4cf03fd1b4715828f88eba5c2aa8
Tobias Wennergren <wennergr@gmail.com>
2014-08-31 13:41:08 -0700

    Installation is now checking more needed dependencies

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@ set -e
 # Install lrzsz if needed
 brew list -1 | grep lrzsz > /dev/null || brew install lrzsz
 
+# Check for needed depdendencies
+hash rz 2>/dev/null || { echo >&2 "Zmodem is required but it's not installed. Aborting."; exit 1; }
+hash sz 2>/dev/null || { echo >&2 "Zmodem is required but it's not installed. Aborting."; exit 1; }
+hash osascript 2>/dev/null || { echo >&2 "Osascript is required but it's not installed. Aborting."; exit 1; }
+
 INSTALL_DIR=/usr/local/bin
 sudo mkdir -p ${INSTALL_DIR}
 
