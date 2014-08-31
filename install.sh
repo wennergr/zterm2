3787b1601635adb1ce17cafdf9cdc20c195b739b
Tobias Wennergren <wennergr@gmail.com>
2014-08-31 13:35:50 -0700

    Changed installation order

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-INSTALL_DIR=/usr/local/bin
-sudo mkdir -p ${INSTALL_DIR}
-
 # Install lrzsz if needed
 brew list -1 | grep lrzsz > /dev/null || brew install lrzsz
+
+INSTALL_DIR=/usr/local/bin
+sudo mkdir -p ${INSTALL_DIR}
 
 sudo install -m 555 ziterm2-recv.sh ${INSTALL_DIR}
 sudo install -m 555 ziterm2-send.sh ${INSTALL_DIR}
